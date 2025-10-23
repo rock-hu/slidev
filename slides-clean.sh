d7067f708f0f30c70ff7f55ef3a9968eebae1928
Rock Hu <rock.c.c.hu@gmail.com>
2025-10-24 01:00:52 +0800

    ci: slidev build --out public/*

--- slides-clean.sh
+++ slides-clean.sh
@@ -3,6 +3,5 @@ ls -l public
 while IFS="," read -r slide
 do
     echo $slide
-    rm -rf ${slide} 
     rm -rf public/${slide} 
 done <  topics.csv
--- slides-move.sh
+++ slides-move.sh
@@ -2,8 +2,6 @@
 while IFS="," read -r slide
 do
     echo $slide
-    mv -f ${slide} public
-    pwd
     ls -l public/${slide}
 done <  topics.csv
 
